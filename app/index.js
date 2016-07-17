6c7640cacc940cee6f8cccd7dc86af624690b910
Hannah Wolfe <erisds@gmail.com>
2016-07-17 17:47:43 +0100

    Part 2: Nested components etc

--- app/index.js
+++ app/index.js
@@ -1,15 +1,76 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
-   render: function () {
+/**
+ * REACT FIRST
+ * Focused
+ * Independent
+ * Reusable
+ * Small
+ * Testable
+ */
+
+var USER_DATA = {
+    name: "Hannah Wolfe",
+    username: "erisds",
+    image: "https://avatars2.githubusercontent.com/u/101513?v=3&s=460"
+};
+
+var ProfilePic = React.createClass({
+    render: function () {
+        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    }
+});
+
+var Link = React.createClass({
+    changeURL: function () {
+        console.log('CHANGING URL')
+        window.location.replace(this.props.href);
+    },
+    render: function () {
         return (
-            <div>Hello ReactJS Program!</div>
-        )
-   }
+            <span
+                style={{color: 'blue', cursor: 'pointer'}}
+                onClick={this.changeURL}>
+                {this.props.children}
+            </span>
+        );
+    }
+});
+
+var ProfileLink = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Link href={'https://github.com/' + this.props.username}>
+                    {this.props.username}
+                </Link>
+            </div>
+        );
+    }
+});
+
+var ProfileName = React.createClass({
+    render: function () {
+        return (
+            <div>{this.props.name}</div>
+        );
+    }
+});
+
+var Avatar = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        );
+    }
 });
 
 ReactDOM.render(
-    <Hello />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 );
